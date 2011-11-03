6b15c17145d85cb0cac098c185538b0d0f3e598e
Erin Sheldon <erin.sheldon@gmail.com>
2011-11-03 12:20:25 -0400

    updated cost projections, nodes and tb per year

--- hep-early-career-2011/calculate-computing.py
+++ hep-early-career-2011/calculate-computing.py
@@ -1,19 +1,24 @@
-# we need 133 TB
-# we need 122 2010 equivalent CPUs
-#
-# we have extrapolated to 2015 numbers from Ernst's projections
-#    $/TB $/CPU
-#2010 437 3000
-#2011 350 2368
-#2012 280 1956
-#2013 218 1551
-#2014 175 1250
-#---------------
-#2015 131 948
-#
-# if we want fixed cost, we need a constant for
-# the number times the rate, which means Ni = c/ri
-# and c = Ntot/sum(1/ri)
+"""
+Calculate number of nodes and TB per year at fixed spending
+per year.  Not optimal.
+
+  we need 133 TB
+  we need 122 2010 equivalent CPUs
+ 
+  we have extrapolated to 2015 numbers from Ernst's projections
+     $/TB $/CPU
+ 2010 437 3000
+ 2011 350 2368
+ 2012 280 1956
+ 2013 218 1551
+ 2014 175 1250
+ ---------------
+ 2015 131 948
+ 
+  if we want fixed cost, we need a constant for
+  the number times the rate, which means Ni = c/ri
+  and c = Ntot/sum(1/ri)
+"""
 import numpy
 
 
@@ -45,20 +50,24 @@ def print_table(years,
     d['CPUcost'] = 0
     fmt='%(label)s & %(diskTB)d & %(diskcost)d & %(nCPU)d & %(CPUcost)d \\\\' 
     for i,year in enumerate(years):
+        diskcost = round(const_cost_disk,-2)
+        CPUcost  = round(const_cost_CPUs,-3)
         l={'label':year,
            'diskTB':TB_eachyear[i],
-           'diskcost':const_cost_disk,
+           'diskcost':diskcost,
            'nCPU':CPUs_eachyear[i],
-           'CPUcost':const_cost_CPUs}
+           'CPUcost':CPUcost}
 
         print fmt % l
 
         d['diskTB'] += TB_eachyear[i]
-        d['diskcost'] += const_cost_disk
+        d['diskcost'] += diskcost
 
         d['nCPU'] += CPUs_eachyear[i]
-        d['CPUcost'] += const_cost_CPUs
+        d['CPUcost'] += CPUcost
 
+    d['diskcost'] = round(d['diskcost'],-2)
+    d['CPUcost'] = round(d['CPUcost'],-3)
 
     print '\\hline'
     print '\\relax\\\\[-1.7ex]'
@@ -72,7 +81,7 @@ def print_table(years,
     performance level of a node purchased in 2010. As the performance per node will
     increase over time the actual number of compute nodes after 3 years will be
     significantly smaller (probably O(70)), providing a combined performance of
-    O(122) 2010 equivalent nodes. Prices include 40\% bulk discounts from
+    O(120) 2010 equivalent nodes. Prices include 40\% bulk discounts from
     purchasing through the RHIC ATLAS Computing Facility at BNL. {\\bf Power,
     cooling and maintence will be provided at no extra cost.} Although we list
     storage purchases per year, storage most likely be bought in two or
@@ -87,7 +96,7 @@ def get_const_cost(ntot, rates):
     return ntot/rates_inv_sum
 
 def main():
-    years=numpy.array([2012, 2013, 2014,2015])
+    years=numpy.array([2012,2013,2014,2015])
     cost_perTB_eachyear = numpy.array([280.,218.75,175.,131.25],dtype='f8')
     cost_perCPU_eachyear = numpy.array([1956.5217,1551.724,1250., 948.2758],dtype='f8')
 
@@ -99,11 +108,6 @@ def main():
 
     CPUs_eachyear = const_cost_CPUs/cost_perCPU_eachyear
     TB_eachyear = const_cost_disk/cost_perTB_eachyear
-
-    print 'CPUs by year:',CPUs_eachyear
-    print 'total CPUs: %d' % CPUs_eachyear.sum()
-    print 'TB by year:',TB_eachyear
-    print 'total TB: %d' % TB_eachyear.sum()
 
     print_table(years,
                 const_cost_CPUs, CPUs_eachyear,
---
+++ hep-early-career-2011/project-costs.py
@@ -0,0 +1,40 @@
+import numpy
+import biggles
+import esutil as eu
+
+year=numpy.array([2010,2011,2012,2013,2014])
+cpucost = 45000./numpy.array([15,19,23,29,36],dtype='f8')
+diskcost = 7000./numpy.array([16,20,25,32,40],dtype='f8')
+
+plt=biggles.FramedPlot()
+
+cpup=biggles.Points(year, cpucost, type='filled circle',color='darkgreen')
+cpup.label = '\$/2010 equiv. node'
+diskp=biggles.Points(year, diskcost, type='filled diamond',color='blue')
+diskp.label = '\$/TB'
+
+
+cpu2015 = eu.stat.interplin(cpucost, year, 2015.)
+disk2015 = eu.stat.interplin(diskcost, year, 2015.)
+
+print 'cpu  2015:',cpu2015
+print 'disk 2015:',disk2015
+
+cpu2015p = biggles.Points([2015], cpu2015, type='filled circle',color='red')
+cpu2015p.label = 'extrapolated'
+disk2015p = biggles.Points([2015], disk2015, type='filled diamond',color='red')
+disk2015p.label = 'extrapolated'
+
+key=biggles.PlotKey(0.9,0.9,[cpup,cpu2015p,diskp,disk2015p], halign='right')
+
+plt.add(cpup,cpu2015p,diskp,disk2015p,key)
+plt.show()
+
+print year
+print diskcost,disk2015[0]
+print cpucost,cpu2015[0]
+
+for i in xrange(year.size):
+    print '%d %d %d' % (year[i],diskcost[i],cpucost[i])
+print '-'*70
+print '%d %d %d' % (2015,disk2015[0],cpu2015[0])
