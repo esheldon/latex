fce1498eb6ec8b1c79b5539378d58d20c740f562
Erin Sheldon <erin.sheldon@gmail.com>
2012-03-20 15:29:36 -0400

    defining mask bounds

--- ts10/data/par2table.py
+++ ts10/data/par2table.py
@@ -1,6 +1,12 @@
+import sys
 import sdsspy
 
-data=sdsspy.yanny.readone('boss_survey.par')
+if len(sys.argv) < 2:
+    print 'usage: par2table.py par > tab'
+    sys.exit(1)
+
+f=sys.argv[1]
+data=sdsspy.yanny.readone(f)
 
 f='%s & %0.2f & %0.2f & %0.2f & %0.2f & %0.2f'
 for i,t in enumerate(data):
