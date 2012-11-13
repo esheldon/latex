b004f8ed4b4fa329625895cda9482278ce5cf2af
Erin Sheldon <erin.sheldon@gmail.com>
2012-11-13 14:03:27 -0500

    updated budget prediction

--- hep-early-career-2012/calculate-computing.py
+++ hep-early-career-2012/calculate-computing.py
@@ -1,18 +1,19 @@
-time_per_obj_cpu=1.0
-
-# 2 for two GPUs
-speedup=170.*2
-time_per_obj_gpu = time_per_obj_cpu/speedup
+time_per_obj=0.011630
 
 nobj=500000000.
 nexp=10.
 ntotal=nobj*nexp
 
-time_total = time_per_obj_gpu*ntotal
-
+time_total = time_per_obj*ntotal
 
 desired_time=2.5*7*24*60*60
 
-nmachines=time_total/desired_time
+# divide by two for two gpus
+nmachines=time_total/desired_time/2
 
 print 'nmachines:',nmachines
+
+cost_per_machine=8800.
+total_cost=round(nmachines)*cost_per_machine
+
+print 'cost:',total_cost
