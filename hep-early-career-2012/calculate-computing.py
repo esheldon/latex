5ca9f801e9208bbc7cccb5f207636ae828e7390c
Erin Sheldon <erin.sheldon@gmail.com>
2012-11-16 16:21:41 -0500

    put back in appendix 5

--- hep-early-career-2012/calculate-computing.py
+++ hep-early-career-2012/calculate-computing.py
@@ -1,3 +1,10 @@
+import sys
+if len(sys.argv) < 2:
+    print 'calculate-computing ngpu'
+    sys.exit(1)
+
+ngpu=int(sys.argv[1])
+
 time_per_obj=0.011630
 
 nobj=500000000.
@@ -9,11 +16,22 @@ time_total = time_per_obj*ntotal
 desired_time=2.5*7*24*60*60
 
 # divide by two for two gpus
-nmachines=time_total/desired_time/2
+nmachines=int(round(time_total/desired_time/ngpu))
 
 print 'nmachines:',nmachines
 
-cost_per_machine=8800.
+#cost_per_machine=8800.
+base_gpu_cost=1800.
+gpu_cost=ngpu*base_gpu_cost
+base_cost=5200.
+cost_per_machine=base_cost + gpu_cost
 total_cost=round(nmachines)*cost_per_machine
 
+print 'base machine cost:',base_cost
+print 'base gpu cost:',base_gpu_cost
+print 'cost per:',cost_per_machine
 print 'cost:',total_cost
+
+print 'disk per machine:',130./nmachines
+
+print 'cost increase:',cost_per_machine/base_cost
