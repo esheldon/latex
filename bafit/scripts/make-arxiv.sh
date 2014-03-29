709bdba64b7c77163b726423a40f16cf92a4ac9b
Erin Sheldon <erin.sheldon@gmail.com>
2014-03-29 18:34:39 -0400

    arxiv maker

--- bafit/scripts/make-arxiv.sh
+++ bafit/scripts/make-arxiv.sh
@@ -4,8 +4,8 @@ dir=bafit-arxiv
 mkdir -pv $dir
 mkdir -pv $dir/figures
 
-cp -L -fv bafit_mnras.tex \
-          bafit_mnras.bbl \
+cp -L -fv bafit.tex \
+          bafit.bbl \
           mn2e.bst \
           mn2e.cls \
           newcommands.tex \
